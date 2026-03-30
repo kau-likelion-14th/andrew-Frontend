3d49c3994903489aac96d856dd21f513721080d2
TomorrowIppo <bjk1624@gmail.com>
2026-03-30 19:19:49 +0900

    feat: mainpage ui

--- test/src/App.js
+++ test/src/App.js
@@ -1,7 +1,32 @@
+// import React from 'react';
+// import './App.css';
+// import Header from './components/Header'
+// import MainPage from './pages/MainPage'
+
+// function App() {
+//   return (
+//     <div>
+//       <Header/>
+//       <h1></h1>
+//     </div>
+//   );
+// }
+
+// export default App;
+import {Routes,Route} from "react-router-dom"
+import './App.css';
+import Header from './components/Header'
+import MainPage from './pages/MainPage/MainPage'
+
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <Header/>
+
+      <Routes>
+        <Route path='/' element={<MainPage/>} />
+      </Routes>
+
     </div>
   );
 }
