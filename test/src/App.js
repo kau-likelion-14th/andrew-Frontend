a017d479fef729798d01127faff26763d19e1234
TomorrowIppo <bjk1624@gmail.com>
2026-03-31 18:21:08 +0900

    feat: implement Footer UI Component

--- test/src/App.js
+++ test/src/App.js
@@ -16,6 +16,7 @@
 import {Routes,Route} from "react-router-dom"
 import './App.css';
 import Header from './components/Header'
+import Footer from './components/Footer'
 import MainPage from './pages/MainPage/MainPage'
 
 function App() {
@@ -26,7 +27,8 @@ function App() {
       <Routes>
         <Route path='/' element={<MainPage/>} />
       </Routes>
-
+      
+      <Footer/>
     </div>
   );
 }
